b1852fa14edd80ea05cf048acb3268c1e486c2cb
Siigmaf <faby.tbe@gmail.com>
2022-07-04 19:37:07 -0300

    corrigindo erros requisito 03

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-const container=document.getElementById('pixel-board');
+/*const container=document.getElementById('pixel-board');
 
 function pixelArt(size){
     container.style.setProperty('--size',size)
     for (let i = 0; i < size * size; index+=1) {
         const div = document.createElement('div')
+        div.className='pixel'
         div.classList.add('pixel')
         container.appendChild(div)
     }
 }
-pixelArt(0);
+pixelArt(0);*/
